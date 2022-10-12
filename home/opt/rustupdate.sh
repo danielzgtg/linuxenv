ee202a66ca77ebe66cdfb5587886a5a21316260a
Daniel Tang <danielzgtg.opensource@gmail.com>
2022-10-12 10:00:33 -0400

    Update rustup settings.toml hash

--- home/opt/rustupdate.sh
+++ home/opt/rustupdate.sh
@@ -2,14 +2,14 @@
 set -e
 cd ~/.rustup
 rm -rf downloads tmp
-if ! sha256sum settings.toml | grep -q 47dd8d63991c4b85dd223d77672d7ee3fe0ac0d808d082fe03edf3c7bc5ea9cd; then
+if ! sha256sum settings.toml | grep -q 3b34694c78cf6aaa4b1c998892e03a65a766b4db6a54417063ff18910682b9d9; then
   echo 'settings.toml Hash Error'
   exit 1
 fi
 if ! ls -1 toolchains/ | wc -l | grep -q '^1$'; then
   echo 'Too many toolchains'
   exit 1
-exit
+fi
 ~/.cargo/bin/rustup update
 tldr --update
 ~/.cargo/bin/cargo-install-update install-update -af || true
