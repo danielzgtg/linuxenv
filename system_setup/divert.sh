05f97ba72f4488ae0f3cf76081a539b428daf163
Daniel Tang <danielzgtg.opensource@gmail.com>
2022-10-13 06:55:59 -0400

    Make jp_us a variant

--- system_setup/divert.sh
+++ system_setup/divert.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+cd ../system
 dpkg-divert --local --rename --divert '/etc/apt/apt.conf.d/#50appstream' /etc/apt/apt.conf.d/50appstream
 dpkg-divert --local --rename --divert '/usr/share/X11/xkb/symbols/#level3' /usr/share/X11/xkb/symbols/level3
+dpkg-divert --local --rename --divert '/usr/share/X11/xkb/symbols/#jp' /usr/share/X11/xkb/symbols/jp
 cp -T '/usr/share/X11/xkb/symbols/#level3' /usr/share/X11/xkb/symbols/level3
+cp -T usr/share/X11/xkb/symbols/jp /usr/share/X11/xkb/symbols/jp
 chmod 644 /usr/share/X11/xkb/symbols/level3
+chmod 644 /usr/share/X11/xkb/symbols/jp
 echo Done
