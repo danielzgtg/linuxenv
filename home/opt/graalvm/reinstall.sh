85f2b04b8387bbf0426b0f887067d91cfa1b3613
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-01-29 15:45:28 -0500

    Update GraalVM 22.3.0 -> 22.3.1

--- home/opt/graalvm/reinstall.sh
+++ home/opt/graalvm/reinstall.sh
@@ -2,11 +2,11 @@
 set -e
 rm -f current
 rm -rf graalvm*
-VERSION='22.3.0'
+VERSION='22.3.1'
 TARGET='graalvm-ce-java19-'"$VERSION"
 NAME='graalvm-ce-java19-linux-amd64-'"$VERSION"'.tar.gz'
 wget 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-'"$VERSION"/"$NAME"
-if ! sha256sum "$NAME" | grep -q ae9cb1afe327d49a8c049ab588090838e622d9d832b9a1c0523821a6f38d6b4d; then
+if ! sha256sum "$NAME" | grep -q 7cd99d805e7a8b7d4c4576802fb107fb862944e47ce5f2e4f37c0f469a70dd2f; then
   echo 'Hash Error'
   exit 1
 fi
