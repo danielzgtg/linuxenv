3a03c2d060c6d55a4d45d765b5e89003d946486b
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-03-13 15:04:08 -0400

    Add apt sshfs

---
+++ home/sshfshome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#exec sshfs -p 2222 192.168.3.150:/home/home/ ./mnt
+#touch /var/cache/apt/archives/lock; mount -t overlay -o lowerdir=/home/home/mnt,upperdir=/var/cache/apt/archives,workdir=/var/cache/apt/work overlayfs /var/cache/apt/archives; rmdir /var/cache/apt/archives/partial
