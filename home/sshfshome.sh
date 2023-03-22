7bf509f8ff35f9612b147a7220d4a22493021c52
Daniel Tang <danielzgtg.opensource@gmail.com>
2023-03-22 08:54:25 -0400

    Add proper sshfs command

--- home/sshfshome.sh
+++ home/sshfshome.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#exec sshfs -p 2222 192.168.3.150:/home/home/ ./mnt
+#exec sshfs -p 2222 daniel-desktop3.local:/home/home/ ./mnt
+#exec sshfs -p 2222 -o allow_root daniel-desktop3.local:/var/cache/apt/archives ./mnt
 #touch /var/cache/apt/archives/lock; mount -t overlay -o lowerdir=/home/home/mnt,upperdir=/var/cache/apt/archives,workdir=/var/cache/apt/work overlayfs /var/cache/apt/archives; rmdir /var/cache/apt/archives/partial
