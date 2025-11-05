6d3313d291dbc77c786235a52a81d5673a6a9fbc
Daniel Tang <danielzgtg.opensource@gmail.com>
2025-11-04 21:37:45 -0500

    cups: Socket-activate

--- system_setup/systemctl.sh
+++ system_setup/systemctl.sh
@@ -1,4 +1,6 @@
 #!/bin/dash
+systemctl disable cups
+systemctl enable cups.socket
 systemctl disable docker # but still docker.socket-activatable
 systemctl disable containerd # but not docker.service-activatable
 systemctl disable NetworkManager-wait-online.service
