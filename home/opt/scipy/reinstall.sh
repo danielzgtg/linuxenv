f434763015a64ab91add9d1d440567ff565117ca
Daniel Tang <danielzgtg.opensource@gmail.com>
2026-01-20 05:42:17 -0500

    scipy: Enable updating away from system numpy

--- home/opt/scipy/reinstall.sh
+++ home/opt/scipy/reinstall.sh
@@ -3,6 +3,6 @@ set -e
 eatmydata rm -rf venv
 eatmydata python3 -m venv venv --system-site-packages
 . venv/bin/activate
-eatmydata pip3 install numpy scipy matplotlib seaborn pandas
+eatmydata pip3 install -U numpy scipy matplotlib seaborn pandas
 pip freeze
 exec sync
