953d0176e349eb99416c7348ae00f552592b1477
Daniel Tang <danielzgtg.opensource@gmail.com>
2026-02-15 20:43:26 -0500

    z3: Add array stuff

--- home/opt/z3-solver/init_z3_solver.py
+++ home/opt/z3-solver/init_z3_solver.py
@@ -13,3 +13,12 @@ Pair = PairSort(IntSort())
 pair = lambda x, y: Pair.new(x, y)
 s = Solver()  # For when solve() ain't enough
 o = Optimize()
+fp = Fixedpoint()
+a = Array("a", IntSort(), IntSort())
+def init_a(vals):
+    # https://github.com/Z3Prover/z3/pull/8624 was declined
+    if not isinstance(vals, list):
+        raise TypeError
+    if a.domain() != IntSort():
+        raise ValueError
+    return [a[i] == x for i, x in enumerate(vals)]
